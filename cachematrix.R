5b525a2a81ce185da54bdf1194d9f0aabea47a39
Jon Farland <jon.farland@dnvgl.com>
2016-02-22 02:00:05 -0500

    adjusted UDFs

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Pretty Straightforward to see how the example functions can  be applied to matrix objects
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(g, ...)  {
+  inv <- g$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- g$get()
+  inverse <- solve(data)
+  g$setinverse(inverse)
+  inverse
 }
